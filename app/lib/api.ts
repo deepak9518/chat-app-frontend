8884bca675e147cd99ceac02f0fa129ff64f68ad
Deepak <Deepaksharmabalu1@gmail.com>
2026-05-06 19:52:33 +0530

    update backend url in frontend

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000",
+  baseURL: "https://chat-app-backend-omega-beryl-25.vercel.app",
   withCredentials: true,
   headers: {
     "Cache-Control": "no-cache",
