07df51ccacb166ce4041408a4c86a551e29a7864
Deepak <Deepaksharmabalu1@gmail.com>
2026-05-06 20:16:32 +0530

    update backend

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://chat-app-backend-omega-beryl-25.vercel.app",
+  baseURL: "https://chat-app-backend-2vyp.onrender.com/",
   withCredentials: true,
   headers: {
     "Cache-Control": "no-cache",
